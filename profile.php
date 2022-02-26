bacfef4fc79225302bbb7ed13cca55744cb98cf9
kenji-manabe <81913108+kenji-manabe@users.noreply.github.com>
2022-02-27 03:41:14 +0900

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,25 @@
+<?php
+
+$profile = new Profile();
+$profile->present();
+
+class Profile
+{
+  public function present()
+  {
+    echo "Name:";
+    echo " 眞鍋 賢司";
+    
+    echo "Job:";
+    echo "　プログラマ";
+    
+    echo "Company:";
+    echo "　KeepAlive株式会社";
+    
+    echo "sellingPoint:";
+    echo " 身長180センチ";
+    
+    echo "charmingPoint:";
+    echo " 笑うとえくぼができる。そして、身長180センチ。";
+  }
+}
